be56d470f44ff99af0794ed143da39d13d062bec
Mohamed  Heiba <mohamed222101223@ksiu.edu.eg>
2025-09-24 17:44:17 +0300

    Delete flutter_ml directory

--- flutter_ml/lib/UI_UX/Colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color darkTheme = Color(0xFF1A1A1A);
-  static const Color lightTheme = Color(0xFFFFFFFF);
-  static const Color primaryColor = Color(0xFF212121);
-  static const Color secondaryColor = Colors.blue;
-  static const Color currentTheme = darkTheme;
-}
